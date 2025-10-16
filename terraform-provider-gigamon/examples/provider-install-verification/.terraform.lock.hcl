9113b4a37a4b30f3c555a77185d7664e69830cf2
Jana Pathangi <jana.pathangi@gigamon.com>
2025-10-16 15:36:36 +0000

    An initial sample code to get the development started

---
+++ terraform-provider-gigamon/examples/provider-install-verification/.terraform.lock.hcl
@@ -0,0 +1,9 @@
+# This file is maintained automatically by "terraform init".
+# Manual edits may be lost in future updates.
+
+provider "local/gigamon/fm" {
+  version = "1.0.0"
+  hashes = [
+    "h1:k1Skc8Fcx+J2s1iJzJ4rfZr4bSDj3+D8S9vERW6UEvc=",
+  ]
+}
---
+++ terraform-provider-gigamon/internal/provider/provider.go
@@ -0,0 +1,65 @@
+// Provider package for Gigamon FM product. Implements the provider for cloud functioanlities
+
+package provider
+
+import (
+	"context"
+
+	"github.com/hashicorp/terraform-plugin-framework/datasource"
+	"github.com/hashicorp/terraform-plugin-framework/ephemeral"
+	"github.com/hashicorp/terraform-plugin-framework/function"
+	"github.com/hashicorp/terraform-plugin-framework/provider"
+	"github.com/hashicorp/terraform-plugin-framework/resource"
+)
+
+// Ensure GigamonProvider satisfies various provider interfaces.
+var _ provider.Provider = &GigamonProvider{}
+
+// GigamonProvider is the implementation of Gigamon Provider
+type GigamonProvider struct {
+	// version is set to the provider version on release, "dev" when the
+	// provider is built and ran locally, and "test" when running acceptance
+	// testing.
+	version string
+}
+
+// GigamonProviderModel describes the provider data model.
+type GigamonProviderModel struct {
+}
+
+func (p *GigamonProvider) Metadata(ctx context.Context, req provider.MetadataRequest, resp *provider.MetadataResponse) {
+	resp.TypeName = "gigamon"
+	resp.Version = p.version
+}
+
+func (p *GigamonProvider) Schema(ctx context.Context, req provider.SchemaRequest, resp *provider.SchemaResponse) {
+	return
+}
+
+func (p *GigamonProvider) Configure(ctx context.Context, req provider.ConfigureRequest, resp *provider.ConfigureResponse) {
+	return
+}
+
+func (p *GigamonProvider) Resources(ctx context.Context) []func() resource.Resource {
+	return nil
+}
+
+func (p *GigamonProvider) EphemeralResources(ctx context.Context) []func() ephemeral.EphemeralResource {
+	return nil
+}
+
+func (p *GigamonProvider) DataSources(ctx context.Context) []func() datasource.DataSource {
+	return nil
+}
+
+func (p *GigamonProvider) Functions(ctx context.Context) []func() function.Function {
+	return nil
+}
+
+func New(version string) func() provider.Provider {
+	return func() provider.Provider {
+		return &GigamonProvider{
+			version: version,
+		}
+	}
+}
