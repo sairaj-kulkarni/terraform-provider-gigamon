2cb3a159d5273298ce86b81c69ce269cc2b37578
Janardhanan <jana.pathangi@gigamon.com>
2026-02-27 08:26:50 +0000

    update to the build script

--- terraform-provider/build/build.py
+++ terraform-provider/build/build.py
@@ -16,7 +16,7 @@ from zipfile import ZipFile, ZIP_DEFLATED
 
 # For now have a hard coded list of versions, that we want to permanently keep
 # irrespective of the time when it was created etc.
-PERMANANT_VERSION = ["6.14.225"]
+PERMANANT_VERSION = []
 
 ARTIFACT_DIR = "fm_terraform_provider/terraform-provider/artifacts"
 GPG_DIR = "fm_terraform_provider/terraform-provider/build/gpg_keys"
