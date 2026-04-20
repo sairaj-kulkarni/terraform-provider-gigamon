cbf408beed12863fb62a21887729d102e37e7975
vgopu <vijaykumar.gopu@gigamon.com>
2026-04-20 12:33:38 +0000

    FM-62553 : Secure Tunnel Certs Data Source

---
+++ internal/securetunnelcertsdatasources/cloud_ca_cert.go
@@ -0,0 +1,201 @@
+// Copyright (c) Gigamon, Inc.
+
+// Implements the Data Source for reading Cloud CA Certificate details from FM
+
+package securetunnelcertsdatasources
+
+import (
+	"context"
+	"encoding/json"
+	"fmt"
+	"strings"
+
+	"github.com/hashicorp/terraform-plugin-framework-validators/stringvalidator"
+	"github.com/hashicorp/terraform-plugin-framework/attr"
+	"github.com/hashicorp/terraform-plugin-framework/datasource"
+	"github.com/hashicorp/terraform-plugin-framework/datasource/schema"
+	"github.com/hashicorp/terraform-plugin-framework/schema/validator"
+	"github.com/hashicorp/terraform-plugin-framework/types"
+
+	"terraform-provider-gigamon/internal/fmclient"
+)
+
+// Ensure provider defined types fully satisfy framework interfaces
+var _ datasource.DataSource = &CloudCaCertDataSource{}
+var _ datasource.DataSourceWithConfigure = &CloudCaCertDataSource{}
+
+func NewCloudCaCertDataSource() datasource.DataSource {
+	return &CloudCaCertDataSource{}
+}
+
+type CloudCaCertDataSource struct {
+	fmClient *fmclient.FmClient
+}
+
+// CloudCaCertDSModel describes the data source model
+type CloudCaCertDSModel struct {
+	Alias        types.String `tfsdk:"alias"`
+	Certificates types.List   `tfsdk:"certificates"`
+}
+
+// FM response from GET API
+type fmCaCertResponse struct {
+	Alias        string           `json:"alias"`
+	Certificates []fmCertMetadata `json:"certificates"`
+}
+
+type fmCertMetadata struct {
+	DateNotAfter  string `json:"dateNotAfter"`
+	DateNotBefore string `json:"dateNotBefore"`
+	Algorithm     string `json:"algo"`
+	Version       int64  `json:"version"`
+	Issuer        string `json:"issuer"`
+	Name          string `json:"name"`
+}
+
+var caCertMetadataType = types.ObjectType{
+	AttrTypes: map[string]attr.Type{
+		"date_not_after":  types.StringType,
+		"date_not_before": types.StringType,
+		"algorithm":       types.StringType,
+		"version":         types.Int64Type,
+		"issuer":          types.StringType,
+		"name":            types.StringType,
+	},
+}
+
+func (d *CloudCaCertDataSource) Metadata(ctx context.Context, req datasource.MetadataRequest, resp *datasource.MetadataResponse) {
+	resp.TypeName = req.ProviderTypeName + "_cloud_ca_cert"
+}
+
+func (d *CloudCaCertDataSource) Schema(ctx context.Context, req datasource.SchemaRequest, resp *datasource.SchemaResponse) {
+	resp.Schema = schema.Schema{
+		MarkdownDescription: "Data source to read Cloud CA Certificate details from FM.",
+
+		Attributes: map[string]schema.Attribute{
+			"alias": schema.StringAttribute{
+				MarkdownDescription: "Alias of the Cloud CA certificate entry in FM",
+				Required:            true,
+				Validators: []validator.String{
+					stringvalidator.LengthAtLeast(1),
+				},
+			},
+			"certificates": schema.ListNestedAttribute{
+				MarkdownDescription: "Certificate metadata from FM",
+				Computed:            true,
+				NestedObject: schema.NestedAttributeObject{
+					Attributes: map[string]schema.Attribute{
+						"date_not_after": schema.StringAttribute{
+							Computed: true,
+						},
+						"date_not_before": schema.StringAttribute{
+							Computed: true,
+						},
+						"algorithm": schema.StringAttribute{
+							Computed: true,
+						},
+						"version": schema.Int64Attribute{
+							Computed: true,
+						},
+						"issuer": schema.StringAttribute{
+							Computed: true,
+						},
+						"name": schema.StringAttribute{
+							Computed: true,
+						},
+					},
+				},
+			},
+		},
+	}
+}
+
+func (d *CloudCaCertDataSource) Configure(ctx context.Context, req datasource.ConfigureRequest, resp *datasource.ConfigureResponse) {
+	if req.ProviderData == nil {
+		return
+	}
+
+	fmClient, ok := req.ProviderData.(*fmclient.FmClient)
+	if !ok {
+		resp.Diagnostics.AddError(
+			"Unexpected Data Source Configure Type",
+			fmt.Sprintf("Expected *fmclient.FmClient, got: %T. Report the issue to Gigamon", req.ProviderData),
+		)
+		return
+	}
+
+	d.fmClient = fmClient
+}
+
+func (d *CloudCaCertDataSource) Read(ctx context.Context, req datasource.ReadRequest, resp *datasource.ReadResponse) {
+	var data CloudCaCertDSModel
+
+	resp.Diagnostics.Append(req.Config.Get(ctx, &data)...)
+	if resp.Diagnostics.HasError() {
+		return
+	}
+
+	alias := strings.TrimSpace(data.Alias.ValueString())
+	if alias == "" {
+		resp.Diagnostics.AddError("Missing alias", "alias must not be empty")
+		return
+	}
+
+	var fmData fmCaCertResponse
+
+	respBytes, err := d.fmClient.DoRequest(
+		ctx,
+		"GET",
+		fmt.Sprintf("api/v1.3/cloud/nodes/caCert/%s", alias),
+		nil,
+		nil,
+		nil,
+		"",
+	)
+	if err != nil {
+		resp.Diagnostics.AddError(
+			"Unable to read Cloud CA Certificate",
+			fmt.Sprintf("Alias %q error: %v", alias, err),
+		)
+		return
+	}
+
+	if err := json.Unmarshal(respBytes, &fmData); err != nil {
+		resp.Diagnostics.AddError(
+			"Unable to parse Cloud CA Certificate response",
+			fmt.Sprintf("Alias %q error: %v", alias, err),
+		)
+		return
+	}
+
+	data.Alias = types.StringValue(fmData.Alias)
+
+	elems := make([]attr.Value, 0, len(fmData.Certificates))
+	for _, cert := range fmData.Certificates {
+		obj, diags := types.ObjectValue(
+			caCertMetadataType.AttrTypes,
+			map[string]attr.Value{
+				"date_not_after":  types.StringValue(cert.DateNotAfter),
+				"date_not_before": types.StringValue(cert.DateNotBefore),
+				"algorithm":       types.StringValue(cert.Algorithm),
+				"version":         types.Int64Value(cert.Version),
+				"issuer":          types.StringValue(cert.Issuer),
+				"name":            types.StringValue(cert.Name),
+			},
+		)
+		if diags.HasError() {
+			resp.Diagnostics.AddError("Failed to build certificate metadata", "")
+			return
+		}
+		elems = append(elems, obj)
+	}
+
+	listVal, diags := types.ListValue(caCertMetadataType, elems)
+	if diags.HasError() {
+		resp.Diagnostics.AddError("Failed to build certificates list", "")
+		return
+	}
+	data.Certificates = listVal
+
+	resp.Diagnostics.Append(resp.State.Set(ctx, &data)...)
+}
---
+++ internal/securetunnelcertsdatasources/cloud_ssl_keys.go
@@ -0,0 +1,204 @@
+// Copyright (c) Gigamon, Inc.
+
+// Implements the Data Source for reading Cloud SSL Keys details from FM
+
+package securetunnelcertsdatasources
+
+import (
+	"context"
+	"encoding/json"
+	"fmt"
+	"strings"
+
+	"github.com/hashicorp/terraform-plugin-framework-validators/stringvalidator"
+	"github.com/hashicorp/terraform-plugin-framework/attr"
+	"github.com/hashicorp/terraform-plugin-framework/datasource"
+	"github.com/hashicorp/terraform-plugin-framework/datasource/schema"
+	"github.com/hashicorp/terraform-plugin-framework/schema/validator"
+	"github.com/hashicorp/terraform-plugin-framework/types"
+
+	"terraform-provider-gigamon/internal/fmclient"
+)
+
+// Ensure provider defined types fully satisfy framework interfaces
+var _ datasource.DataSource = &CloudSSLKeysDataSource{}
+var _ datasource.DataSourceWithConfigure = &CloudSSLKeysDataSource{}
+
+func NewCloudSSLKeysDataSource() datasource.DataSource {
+	return &CloudSSLKeysDataSource{}
+}
+
+type CloudSSLKeysDataSource struct {
+	fmClient *fmclient.FmClient
+}
+
+// CloudSSLKeysDSModel describes the data source model
+type CloudSSLKeysDSModel struct {
+	Alias         types.String `tfsdk:"alias"`
+	KeyStoreAlias types.String `tfsdk:"key_store_alias"`
+	Metadata      types.Object `tfsdk:"metadata"`
+}
+
+// FM response from GET API
+type fmSSLKeyResponse struct {
+	CloudSSLKey fmSSLKey `json:"cloudSslKey"`
+}
+
+type fmSSLKey struct {
+	Alias         string `json:"alias"`
+	KeyStoreAlias string `json:"keyStoreAlias"`
+	InstalledOn   string `json:"installedOn"`
+	Certificate   bool   `json:"certificate"`
+	PrivateKey    bool   `json:"privateKey"`
+	CommonName    string `json:"cn"`
+	Organization  string `json:"o"`
+	Expiry        string `json:"expiry"`
+	KeyType       string `json:"keyType"`
+}
+
+var sslKeyMetadataType = types.ObjectType{
+	AttrTypes: map[string]attr.Type{
+		"common_name":  types.StringType,
+		"organization": types.StringType,
+		"expiry":       types.StringType,
+		"key_type":     types.StringType,
+		"certificate":  types.BoolType,
+		"private_key":  types.BoolType,
+		"installed_on": types.StringType,
+	},
+}
+
+func (d *CloudSSLKeysDataSource) Metadata(ctx context.Context, req datasource.MetadataRequest, resp *datasource.MetadataResponse) {
+	resp.TypeName = req.ProviderTypeName + "_cloud_ssl_keys"
+}
+
+func (d *CloudSSLKeysDataSource) Schema(ctx context.Context, req datasource.SchemaRequest, resp *datasource.SchemaResponse) {
+	resp.Schema = schema.Schema{
+		MarkdownDescription: "Data source to read Cloud SSL Keys (private key + certificate) details from FM.",
+
+		Attributes: map[string]schema.Attribute{
+			"alias": schema.StringAttribute{
+				MarkdownDescription: "Alias of the Cloud SSL Keys entry in FM",
+				Required:            true,
+				Validators: []validator.String{
+					stringvalidator.LengthAtLeast(1),
+				},
+			},
+			"key_store_alias": schema.StringAttribute{
+				MarkdownDescription: "Key store alias",
+				Computed:            true,
+			},
+			"metadata": schema.SingleNestedAttribute{
+				MarkdownDescription: "SSL key metadata from FM",
+				Computed:            true,
+				Attributes: map[string]schema.Attribute{
+					"common_name": schema.StringAttribute{
+						Computed: true,
+					},
+					"organization": schema.StringAttribute{
+						Computed: true,
+					},
+					"expiry": schema.StringAttribute{
+						Computed: true,
+					},
+					"key_type": schema.StringAttribute{
+						Computed: true,
+					},
+					"certificate": schema.BoolAttribute{
+						Computed: true,
+					},
+					"private_key": schema.BoolAttribute{
+						Computed: true,
+					},
+					"installed_on": schema.StringAttribute{
+						Computed: true,
+					},
+				},
+			},
+		},
+	}
+}
+
+func (d *CloudSSLKeysDataSource) Configure(ctx context.Context, req datasource.ConfigureRequest, resp *datasource.ConfigureResponse) {
+	if req.ProviderData == nil {
+		return
+	}
+
+	fmClient, ok := req.ProviderData.(*fmclient.FmClient)
+	if !ok {
+		resp.Diagnostics.AddError(
+			"Unexpected Data Source Configure Type",
+			fmt.Sprintf("Expected *fmclient.FmClient, got: %T. Report the issue to Gigamon", req.ProviderData),
+		)
+		return
+	}
+
+	d.fmClient = fmClient
+}
+
+func (d *CloudSSLKeysDataSource) Read(ctx context.Context, req datasource.ReadRequest, resp *datasource.ReadResponse) {
+	var data CloudSSLKeysDSModel
+
+	resp.Diagnostics.Append(req.Config.Get(ctx, &data)...)
+	if resp.Diagnostics.HasError() {
+		return
+	}
+
+	alias := strings.TrimSpace(data.Alias.ValueString())
+	if alias == "" {
+		resp.Diagnostics.AddError("Missing alias", "alias must not be empty")
+		return
+	}
+
+	var fmResp fmSSLKeyResponse
+
+	respBytes, err := d.fmClient.DoRequest(
+		ctx,
+		"GET",
+		fmt.Sprintf("api/v1.3/cloud/ssl/keys/%s", alias),
+		nil,
+		nil,
+		nil,
+		"",
+	)
+	if err != nil {
+		resp.Diagnostics.AddError(
+			"Unable to read Cloud SSL Keys",
+			fmt.Sprintf("Alias %q error: %v", alias, err),
+		)
+		return
+	}
+
+	if err := json.Unmarshal(respBytes, &fmResp); err != nil {
+		resp.Diagnostics.AddError(
+			"Unable to parse Cloud SSL Keys response",
+			fmt.Sprintf("Alias %q error: %v", alias, err),
+		)
+		return
+	}
+
+	meta := fmResp.CloudSSLKey
+
+	data.Alias = types.StringValue(meta.Alias)
+	data.KeyStoreAlias = types.StringValue(meta.KeyStoreAlias)
+
+	metadata, diags := types.ObjectValue(
+		sslKeyMetadataType.AttrTypes,
+		map[string]attr.Value{
+			"common_name":  types.StringValue(meta.CommonName),
+			"organization": types.StringValue(meta.Organization),
+			"expiry":       types.StringValue(meta.Expiry),
+			"key_type":     types.StringValue(meta.KeyType),
+			"certificate":  types.BoolValue(meta.Certificate),
+			"private_key":  types.BoolValue(meta.PrivateKey),
+			"installed_on": types.StringValue(meta.InstalledOn),
+		},
+	)
+	if diags.HasError() {
+		resp.Diagnostics.AddError("Failed to build SSL keys metadata", "")
+		return
+	}
+	data.Metadata = metadata
+
+	resp.Diagnostics.Append(resp.State.Set(ctx, &data)...)
+}
